9007ba8f5899426d1f30ab277d85ee12d4d4e229
starrothkopf <srothkopf@fwparker.org>
2022-09-08 14:42:43 -0400

    done

---
+++ src/studio1/Average.java
@@ -0,0 +1,23 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("First number:");
+		double n1 = in.nextInt();
+		
+		System.out.println("Second number:");
+		double n2 = in.nextInt();
+		
+		double average = (n1 + n2) / 2;
+		
+		System.out.println("The average of " + n1 + " and " + n2 + " is " + average);
+
+	}
+
+}
